f755fc5cd81b414769092f5d81394b42b24d5312
AsyncBanana <jacobedwardjackson@outlook.com>
2021-05-16 13:18:29 +0000

    You can't use frame rules in a metatag CSP :(

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -48,7 +48,7 @@
 		content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
 	/>-->
 	<!-- Other -->
-	<meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; frame-ancestors 'none';">
+	<meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline';">
 </svelte:head>
 <div class="relative h-screen">
 	<h1 class="fadeInTop fadeIn md:text-9xl text-7xl mt-20 m-auto w-min">Build</h1>
