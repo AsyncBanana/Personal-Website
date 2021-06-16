220b7a713101dc12566e5cee7e3a0f52051ebd55
AsyncBanana <jacobedwardjackson@outlook.com>
2021-06-16 13:41:10 +0000

    Fixed heading order

--- src/components/project.svelte
+++ src/components/project.svelte
@@ -7,7 +7,7 @@
 <div class="m-4 card bg-cover" style={`background-image: url(${ImageUrl});`}>
 <div class={`bg-primary compact ${ImageUrl === '' ? '' : 'bg-center glass'} h-full`}>
 	<div class="card-body">
-		<h3 class="card-title">{Name}</h3>
+		<h2 class="card-title">{Name}</h2>
 		<p>{Description}</p>
         <div class="card-actions">
             <a class="btn" href={`/Projects/${Url}`}>Visit project page</a>
